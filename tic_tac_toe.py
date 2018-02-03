b0761054b1c80d4fbc0be1de7e6ca7e04bfd60a5
David O'Keeffe <david.okeeffe25@gmail.com>
2018-02-03 15:33:43 +0000

    Update to randomly pick who goes first, and add option of replay

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,8 @@
+import random
+
+def choose_first():
+    return random.randint(0,2)
+
 def display_board(board):
     print(board[0][0], board[0][1], board[0][2])
     print(board[1][0], board[1][1], board[1][2])
@@ -8,7 +13,7 @@ def player_input(player, board):
     valid_input = False
     row = None
     column = None
-    while valid_input is False:
+    while not valid_input:
         row = int(input('Please enter a valid row number from 1 - 3: '))
         column = int(input('Please enter a valid column number from 1 - 3: '))
         if row < 1 or row > 3 or column < 1 or column > 3:
@@ -63,11 +68,10 @@ def game_over(board, row, column, symbol):
 def move(player, board, row, column, symbol):
     board[row][column] = symbol
 
-
 def tic_tac_toe():
     print('Welcome to Tic Tac Toe')
     board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-    player = 1
+    player = choose_first()
     game_is_over = False
     while not game_is_over and player < 9:
         symbol = player_symbol(player)
@@ -77,5 +81,25 @@ def tic_tac_toe():
         display_board(board)
         player += 1
 
+def check_replay():
+    valid_input = False
+    replay = None
+    while not valid_input:
+        replay = input("Do you want to play again? Y/N: ")
+        print(replay)
+        if (replay is not "Y") and (replay is not "N"):
+            print("Invalid response. Please enter Y or N.")
+        else:
+            valid_input = True
+    if replay is "Y":
+        return True
+    else:
+        return False
 
-tic_tac_toe()
+def game():
+    replay = True
+    while replay:
+        tic_tac_toe()
+        replay = check_replay()
+
+game()
